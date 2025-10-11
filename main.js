64a3997b35dd895b43cb5970f2945aaa3c0aebd2
ihebchebbi1998tn <iheb.chebbi@lcieducation.net>
2025-10-11 07:43:56 +0100

    ada

---
+++ main.js
@@ -0,0 +1,6 @@
+﻿const {app,BrowserWindow}=require("electron");
+app.whenReady().then(()=>{
+  const w=new BrowserWindow({show:true});
+  w.maximize();
+  w.loadURL("https://attelier.vercel.app");
+});
