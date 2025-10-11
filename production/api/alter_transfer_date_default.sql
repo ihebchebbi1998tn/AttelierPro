4944875acaf877bfdcf320c17b8efbb3bf4270b8
Iheb Chebbi <141638922+ihebchebbi1998tn@users.noreply.github.com>
2025-10-11 20:03:36 +0100

    ddeedde

--- production/api/alter_transfer_date_default.sql
+++
@@ -1,3 +0,0 @@
--- Set default value for transfer_date to automatically use current date
-ALTER TABLE production_ready_products 
-MODIFY COLUMN transfer_date DATE DEFAULT (CURDATE()) COMMENT 'Date when product was transferred to production (auto-set to today if null)';
--- production/api/run_setup_measurements_by_size.php
+++
@@ -1,4 +0,0 @@
-<?php
-header('Location: setup_product_measurements_by_size.php');
-exit();
-?>
